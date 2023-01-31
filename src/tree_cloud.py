a205d4f5e1d7ad8e7eeba4a7aebf0b5df69a3466
Ariana Tse <aytse@stanford.edu>
2023-01-30 19:01:34 -0800

    Remove magic numbers, circle row function

--- src/tree_cloud.py
+++ src/tree_cloud.py
@@ -1,99 +1,71 @@
 import turtle
+import src.doors as doors
+DEFAULT_DIRECTION=0
 
-def fill_circle(size, color):
+def fill_circle(radius, color):
     '''
     Draws a filled in circle
-    : param size: radius of circle
+    : param radius: radius of circle
     : param color: color of circle
     '''
     turtle.penup()
     turtle.color(color)
     turtle.begin_fill()
-    turtle.circle(size)
+    turtle.circle(radius)
     turtle.end_fill()
 
-def draw_cloud(start_pos_x, start_pos_y, tuple(dis1m dis), size, color):
+def row_of_circles(num_circles, radius, color):
+    '''
+    Draws a horizontal row of circles
+    : param num_circles: number of circles in row
+    : param radius: radius of circles
+    : param color: color of circles
+    '''
+    for i in range(num_circles):
+        fill_circle(radius, color)
+        x, y = turtle.pos()
+        turtle.setpos(x + radius, y)
+
+def draw_cloud(start_pos_x, start_pos_y, size, color='white'):
     '''
     Draws a cloud
     : param start_pos_x: starting x-position of turtle (Lower left hand corner of cloud)
     : param start_pos_y: starting y-position of turtle (Lower left hand corner of cloud)
-    : param size: radius of circle
-    : param color: color of circle
+    : param size: radius of circles used to make cloud
+    : param color: color of circles used to make cloud
     '''
     turtle.penup()
     turtle.setpos(start_pos_x, start_pos_y)
+    turtle.setheading(DEFAULT_DIRECTION)
 
-    # Circle 1
-    fill_circle(size, color)
-
-    # Move to position of second circle in cluster (2nd circle, bottom row)
-    turtle.forward(size * (6 / 5))
-    turtle.right(90)
-    turtle.forward(size / 4)
-    turtle.left(90)
-
-    # Circle 2
-    fill_circle(size * 1.1, color)
-
-    # Move to position of third circle in cluster (3rd circle, bottom row)
-    turtle.forward(size * (6 / 5))
-
-    # Circle 3
-    fill_circle(size, color)
-
-    # Move to position of 4th circle in cluster (1st circle, top row)
-    turtle.left(90)
-    turtle.forward(size * (6.5 / 5))
-    turtle.left(90)
-    turtle.forward(size * 2)
-    turtle.right(180)
-
-    smaller_size = size * 4 / 5
-
-    # Circle 4
-    fill_circle(smaller_size, color)
-
-    # Move to position of 5th circle in cluster (2nd circle, top row)
-    turtle.forward(smaller_size * 1.6)
-
-    # Circle 5
-    fill_circle(smaller_size * 1.2, color)
+    row_of_circles(3, size, color)
+    turtle.setpos(start_pos_x+0.5*size, start_pos_y+size)
+    row_of_circles(2, size, color)
 
 
-def draw_tree(start_pos_x, start_pos_y, size):
+def draw_tree(start_pos_x, start_pos_y, trunk_width, trunk_height, trunk_color='brown', foliage_color='green'):
     '''
     Draws a tree
-    : param start_pos_x: starting x-position of turtle (Upper left hand corner of trunk)
-    : param start_pos_y: starting y-position of turtle (Upper left hand corner of trunk)
-    : param size: radius of circle
-    : param color: color of circle
+    : param start_pos_x: starting x-position of turtle (Lower left hand corner of trunk)
+    : param start_pos_y: starting y-position of turtle (Lower left hand corner of trunk)
+    : param trunk_width: width of trunk
+    : param trunk_height: height of trunk
+    : param trunk_color: color of trunk
+    : param foliage_color: color of foliage
     '''
     turtle.penup()
     turtle.setpos(start_pos_x, start_pos_y)
+    turtle.setheading(DEFAULT_DIRECTION)
 
     # Draw Tree trunk
-    turtle.color('brown')
+    turtle.color(trunk_color)
     turtle.begin_fill()
-    turtle.forward(size)
-    turtle.right(90)
-    turtle.forward(3 * size)
-    turtle.right(90)
-    turtle.forward(size)
-    turtle.right(90)
-    turtle.forward(3 * size)
+    doors.draw_rectangle(start_pos_x, start_pos_y, trunk_height, trunk_width)
     turtle.end_fill()
 
-    # Move to top of trunk to draw foliage
-    turtle.left(90)
-    turtle.forward(size * 0.75)
-    turtle.left(90)
-    turtle.forward(size * 0.25)
-    turtle.left(90)
-
-    x, y = turtle.pos()
-
-    # Draw cloud in green as foliage
-    draw_cloud(x, y, size, 'green')
+    # Draw foliage
+    turtle.setheading(DEFAULT_DIRECTION)
+    draw_cloud(start_pos_x-0.5*trunk_width, start_pos_y+0.75*trunk_height, trunk_width, foliage_color)
 
 
 
